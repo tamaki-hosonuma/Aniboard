3ca3c544f078ac82f389b62704a9733b47c60ee0
Tamaki Hosonuma <iidnelknbk@gmail.com>
2021-07-24 13:38:21 +0900

    投稿機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,9 @@
 class UsersController < ApplicationController
-  before_action :set_current_user
   before_action :user
 
   def show
   end
-
-  def edit
-  end
-
+  
   private
   def user
     @user = current_user
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,9 +4,11 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+window.$ = window.jQuery = require('jquery');
+require("packs/raty")
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
