9313bb38d675bcf7f9d5b06a71b91fc465dd816a
Tamaki Hosonuma <iidnelknbk@gmail.com>
2021-10-12 21:15:07 +0900

    RSpec修正

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -69,3 +69,12 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+
+Capybara.register_driver :selenium_chrome_headless do |app|
+  browser_options = ::Selenium::WebDriver::Chrome::Options.new(
+  browser_options.args << '--headless'
+  browser_options.args << '--no-sandbox'
+  browser_options.args << '--disable-gpu'
+  )
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
+end
--- spec/requests/rakuten_search_spec.rb
+++ spec/requests/rakuten_search_spec.rb
@@ -5,17 +5,25 @@ RSpec.describe "RakutenSearches", type: :request do
     @user = create(:user)
     sign_in @user
     @anime = create(:anime)
+    @dvds = RakutenWebService::Books::DVD.search(title: "日常")
   end
 
-  describe "RakutenAPI search DVD" do
-    it "show DVD page successfully" do
-      get rakuten_search_path(anime_id: @anime.id, keyword: "日常")
-      expect(response.body).to include "検索結果"
-    end
+  it "show DVD page successfully" do
+    get rakuten_search_path(anime_id: @anime.id, keyword: "日常")
+    expect(response.body).to include "検索結果"
+  end
 
-    # it "display message not found if it have no dvds" do
-    #   get rakuten_search_path(anime_id: @anime.id, keyword: "にちじょう")
-    #   expect(response.body).to include "見つかりませんでした"
-    # end
+  context "If there are search results" do
+    it "display the number of hits" do
+      get rakuten_search_path(anime_id: @anime.id, keyword: "日常")
+      expect(response.body).to include @dvds.count.to_s
+    end
+  end
+
+  context "If there is no search result" do
+    it "display as 0" do
+      get rakuten_search_path(anime_id: @anime.id, keyword: "にちじょう")
+      expect(response.body).to include "0件"
+    end
   end
 end
