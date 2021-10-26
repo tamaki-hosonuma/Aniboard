1d14494a4848c39773beb2a260e9e4ef9af1d74e
Tamaki Hosonuma <iidnelknbk@gmail.com>
2021-10-26 09:09:02 +0900

    comment機能実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -29,7 +29,7 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:body, :post_id, :user_id)  
+    params.require(:comment).permit(:body, :post_id, :user_id)
   end
 
   def user
