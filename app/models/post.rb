1eadaf14a8ba63de474e1ca7e13e6f995c804632
Tamaki Hosonuma <iidnelknbk@gmail.com>
2021-10-26 09:13:10 +0900

    comment機能実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,13 @@
 class Post < ApplicationRecord
   belongs_to :user
   belongs_to :anime
+  has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   validates :title, presence: true
   validates :anime_title, presence: true
   validates :body, presence: true, length: { minimum: 1, maximum: 1500 }
   validates :rate, numericality: {
     less_than_or_equal_to: 5,
-    greater_than_or_equal_to: 1,
+    greater_than_or_equal_to: 0.5,
   }, presence: true
 end
