6d6a29d7d2e191ad05feec85e0e91a5fdb9a186c
Tamaki Hosonuma <iidnelknbk@gmail.com>
2022-01-06 23:06:34 +0900

    インデント修正

--- config/application.rb
+++ config/application.rb
@@ -19,10 +19,10 @@ module Aniboard
     # the framework and any gems in your application.
     config.generators do |g|
       g.test_framework :rspec, 
-            view_specs: false, 
-            helper_specs: false, 
-            controller_specs: false, 
-            routing_specs: false
+        view_specs: false, 
+        helper_specs: false, 
+        controller_specs: false, 
+        routing_specs: false
     end
   end
 end
